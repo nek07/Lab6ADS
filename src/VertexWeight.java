4457bc3e9d4111e9796dde5bc1d6d7be8d8e0c03
nek0722 <220439@astanait.edu.kz>
2023-05-30 18:38:08 +0600

    created vertexWeight class and implemented getters and constructor

---
+++ src/VertexWeight.java
@@ -0,0 +1,19 @@
+class VertexWeight<Vertex> implements Comparable<VertexWeight> {
+    private Vertex vertex;
+    private double distance;
+
+    public VertexWeight(Vertex vertex, double distance) {
+        this.vertex = vertex;
+        this.distance = distance;
+    }
+
+
+
+    public Vertex getVertex() {
+        return vertex;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+}
