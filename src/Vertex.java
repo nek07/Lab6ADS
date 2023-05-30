c529b14b066e06814fd9ff778fecdc728ce652ee
nek0722 <220439@astanait.edu.kz>
2023-05-30 14:25:08 +0600

    implemented adjacentVertex method in vertex class

--- src/Vertex.java
+++ src/Vertex.java
@@ -1,2 +1,9 @@
+import java.util.Map;
+
 public class Vertex<V> {
+    private V data;
+    private Map<Vertex<V>, Double> adjacentVertices;
+    private void AdjacentVertex(Vertex<V> destination, double weight){
+        adjacentVertices.put(destination,weight);
+    }
 }
