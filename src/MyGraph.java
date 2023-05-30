3617db2026e5a18cfb29c1008498accff144df87
nek0722 <220439@astanait.edu.kz>
2023-05-30 14:36:04 +0600

    implemented constructor of the MyGraph class and addVertex method

--- src/MyGraph.java
+++ src/MyGraph.java
@@ -1,2 +1,15 @@
-public class MyGraph {
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class MyGraph<Vertex> {
+private Map<Vertex, List<Edge<Vertex>>> map;
+    public MyGraph() {
+        map = new HashMap<>();
+    }
+
+    public void addVertex(Vertex vertex) {
+        map.put(vertex, new LinkedList<>());
+    }
 }
