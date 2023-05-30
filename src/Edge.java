8674140fddc339e5eea48e51823b26723d6d92c3
nek0722 <220439@astanait.edu.kz>
2023-05-30 14:29:15 +0600

    implemented constructor of the edge class

--- src/Edge.java
+++ src/Edge.java
@@ -1,2 +1,15 @@
-public class Edge {
+public class Edge<Vertex> {
+
+    private Vertex source;
+    private Vertex dest;
+    private Double weight;
+    public Edge(Vertex source, Vertex dest, Double weight) {
+        this.source = source;
+        this.dest = dest;
+        this.weight = weight;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
 }
