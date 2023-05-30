956df54f223eec26b059119325c6d4c44f866935
nek0722 <220439@astanait.edu.kz>
2023-05-30 17:03:51 +0600

    implemented addEdge, addDirectedEdge and removeDirected edge methods

--- src/MyGraph.java
+++ src/MyGraph.java
@@ -11,19 +11,38 @@ private Map<Vertex, List<Edge<Vertex>>> map;
 
     public void addVertex(Vertex vertex) {
         map.put(vertex, new LinkedList<>());
+
     }
     private void validateVertex(Vertex index) {
         if (!map.containsKey(index)) {
             throw new IllegalArgumentException("Vertex " + index + " is out of the range");
         }
     }
+    public void addEdge(Vertex source, Vertex destination, double weight) {
+        validateVertex(source);
+        validateVertex(destination);
+        addDirectedEdge(source, destination, weight);
+        addDirectedEdge(destination, source, weight);
+    }
+
+    private void addDirectedEdge(Vertex source, Vertex destination, double weight) {
+        List<Edge<Vertex>> edges = map.getOrDefault(source, new LinkedList<>());
+        edges.add(new Edge(source,destination, weight));
+        map.put(source, edges);
+    }
+
+
     public void removeEdge(Vertex source, Vertex destination) {
         validateVertex(source);
         validateVertex(destination);
-        List<Edge<Vertex>> neighbors = map.get(source);
-        if (neighbors!=null) {
-            neighbors.remove(destination);
-        }
-        map.get(destination).remove(source);
+        removeDirectedEdge(source,destination);
+        removeDirectedEdge(destination,source);
     }
+        public void removeDirectedEdge(Vertex source, Vertex destination) {
+            if (map.containsKey(source)) {
+                List<Edge<Vertex>> edges = map.get(source);
+                edges.removeIf(edge -> edge.getDest() == destination);
+            }
+    }
+
 }
